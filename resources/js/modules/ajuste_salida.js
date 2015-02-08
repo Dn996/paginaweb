a371f6378db5513fe41bb2e1844c5d75795b3f01
eychamba <estyom.1@gmail.com>
2015-02-08 15:03:08 -0500

    Subida inicial para pruebas de aprendizaje de la  la dayana

---
+++ resources/js/modules/ajuste_salida.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    
+    $(function() {
+        $.loadAjaxPanel('#loadproductsviewbtn',{ width: '99%', height: 300 },{top: 35, left: 5});
+        $.loadAjaxPanel('#loadsearchprovbtn', { width: '99%', height: 300 }, {bottom: 0, left: 5});                   
+    });
